251f5e26c6646599e494f9f43af71339e7fbf982
Robin Cordova <robincordova7@gmail.com>
2018-08-22 12:56:59 -0500

    SUbida del proyeycto ANdroid loja tour prueba 1

---
+++ app/src/main/java/com/lojatour/applojatour/LojaTourApp.java
@@ -0,0 +1,15 @@
+package com.lojatour.applojatour;
+
+import android.app.Application;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+public class LojaTourApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+    }
+}
